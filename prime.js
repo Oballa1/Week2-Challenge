871bfa27586a69e11bb1cce47ed86d73474f03cd
Obala <oballasteve4@gmail.com>
2024-04-03 15:37:05 +0300

    checks for prime numbers

--- prime.js
+++ prime.js
@@ -5,7 +5,7 @@ function getPrime(array) {
     // Loop through each value in the input array
     for (const value of array) {
         // Check if the current value meets the conditions for being prime
-        if ((value % 2) > 0 && (value % 3) > 0 && value !== 1 || value === 3 || value === 2) {
+        if ((value % 2) > 0 && (value % 3) > 0 && value !== 1 || value === 3 || value === 2 || value === 5 || value ===7 || value ===11) {
             // If the value is prime, add it to the result array
             result.push(value);
         }
@@ -14,6 +14,6 @@ function getPrime(array) {
     // Return the array containing prime numbers
     return result;
 }
+
 // Call the getPrime function with the example array and log the result
 console.log(getPrime(array));
-
